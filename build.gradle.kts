b3783b4a42e6d6e67100ba14ddb3008a5d330e74
ruitiari <gibsonruitiari@gmail.com>
2022-01-25 15:03:21 +0300

    added + configured dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.6")
+    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.9.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
+    testImplementation("com.google.truth:truth:1.1.3")
     testImplementation(kotlin("test"))
 }
 
